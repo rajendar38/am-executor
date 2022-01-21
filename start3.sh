d9c1b78aa8f6a04115f43c87c745a9fc411d4848
root <root@kmastercent.example.com>
2022-01-21 19:51:00 +0000

    added files

--- start3.sh
+++ start3.sh
@@ -5,6 +5,20 @@ echo $AMX_RECEIVER
 echo $AMX_STATUS 
 echo $AMX_EXTERNAL_URL 
 echo $AMX_LABEL_alertname 
-echo "AMX_LABEL_instance" $AMX_LABEL_instance 
-echo $AMX_ALERT_${1}_LABEL_instance
-echo $AMX_ALERT_LEN
+#echo $AMX_LABEL_instance 
+#echo $AMX_ALERT_${1}_LABEL_instance
+#echo $AMX_ALERT_${2}_LABEL_instance
+echo "length ====================" $AMX_ALERT_LEN
+echo "======================================="
+
+
+main() {
+  for i in $(seq 1 "$AMX_ALERT_LEN"); do
+    ref="AMX_ALERT_${i}_LABEL_instance"
+    echo "$(echo "${!ref}" | cut -d: -f1)"   
+   
+  done
+  wait
+}
+
+main "$@"
